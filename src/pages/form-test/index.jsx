abfc40ebd990b2f15c6d15253ee67efc45375d9d
cys <821566770@qq.com>
2022-12-03 21:43:19 +0800

    feat: form onChange

--- src/pages/form-test/index.jsx
+++ src/pages/form-test/index.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 import FormRender from '@/components/form-render';
 
@@ -6,12 +6,20 @@ import demoSchema from './demo.schema.json';
 
 export default function FormTestPage() {
   const data = useRef({});
+  const [form, setForm] = useState({});
+  function onChange(changedValues, allValues, form) {
+    data.current = allValues;
+    setForm(allValues);
+  }
   return (
     <div>
       FormTestPage
-      {JSON.stringify(data)}
       <br />
-      <FormRender schema={demoSchema} data={data} />
+      data: {JSON.stringify(data)}
+      <br />
+      form: {JSON.stringify(form)}
+      <br />
+      <FormRender schema={demoSchema} data={data} onChange={onChange} />
     </div>
   );
 }
