a6b119cdd97cff58ad93be47a24e08f91a320f55
cys <821566770@qq.com>
2022-12-03 15:05:38 +0800

    init

---
+++ src/pages/form-test/index.jsx
@@ -0,0 +1,11 @@
+import FormRender from "@/components/form-render";
+
+export default function FormTestPage() {
+  return (
+    <div>
+      <h1>Page index</h1>
+      FormTestPage
+      <FormRender />
+    </div>
+  );
+}
