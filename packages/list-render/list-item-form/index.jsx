84667e0301332609d93ed1fe9ab66764dce00171
cys <821566770@qq.com>
2022-12-05 16:31:40 +0800

    feat: list-render

--- packages/list-render/list-item-form/index.jsx
+++ packages/list-render/list-item-form/index.jsx
@@ -1,5 +1,34 @@
-function ListItemForm(props) {
-  return <div>ListItemForm</div>;
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { Modal } from 'antd';
+
+function ListItemForm({}, parentRef) {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('新增');
+  const [form, setForm] = useState({});
+  useImperativeHandle(parentRef, () => ({
+    onShow(formVal, title = '新增') {
+      setForm(formVal || {});
+      setTitle(title);
+      setOpen(true);
+    },
+  }));
+
+  function onOk() {
+    console.log('onOk');
+    onCancel();
+  }
+
+  function onCancel() {
+    setOpen(false);
+  }
+
+  return (
+    <Modal title={title} open={open} onOk={onOk} onCancel={onCancel}>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </Modal>
+  );
 }
 
-export default ListItemForm;
+export default forwardRef(ListItemForm);
--- src/pages/table-test/index.jsx
+++ src/pages/table-test/index.jsx
@@ -1,5 +1,13 @@
+import ListRender from '@packages/list-render';
+
+import demoSchema from './demo.schema.json';
+
 function TableTest(props) {
-  return 'table test';
+  return (
+    <div>
+      <ListRender schema={demoSchema} />
+    </div>
+  );
 }
 
 export default TableTest;
