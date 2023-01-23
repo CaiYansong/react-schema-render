1e26c716a09a4a2b861f3feecde1c0e22eb6138e
CaiYansong <821566770@qq.com>
2023-01-23 20:14:24 +0800

    feat: dayjs

--- .umirc.ts
+++ .umirc.ts
@@ -1,23 +1,33 @@
-import { defineConfig } from 'umi';
-import path from 'path';
+import { defineConfig } from "umi";
+import path from "path";
+// @ts-ignore
+import AntdDayjsWebpackPlugin from "antd-dayjs-webpack-plugin";
 
 export default defineConfig({
   nodeModulesTransform: {
-    type: 'none',
+    type: "none",
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
-    { path: '/form-test', component: '@/pages/form-test' },
-    { path: '/table-test', component: '@/pages/table-test' },
-    { path: '/map-test', component: '@/pages/map-test' },
-    { path: '/mobile-form-test', component: '@/pages/mobile-form-test' },
+    { path: "/", component: "@/pages/index" },
+    { path: "/form-test", component: "@/pages/form-test" },
+    { path: "/table-test", component: "@/pages/table-test" },
+    { path: "/map-test", component: "@/pages/map-test" },
+    { path: "/mobile-form-test", component: "@/pages/mobile-form-test" },
   ],
   fastRefresh: {},
   alias: {
-    '@': 'src/',
-    '@packages': path.resolve(__dirname, 'packages'),
+    "@": "src/",
+    "@packages": path.resolve(__dirname, "packages"),
+  },
+  chainWebpack(memo) {
+    // dayjs 替换 moment
+    memo.plugin("AntdDayjsWebpackPlugin").use(new AntdDayjsWebpackPlugin(), [
+      {
+        preset: "antdv4", // antd 版本
+      },
+    ]);
   },
   devServer: {
-    port: 8100
-  }
+    port: 8100,
+  },
 });
