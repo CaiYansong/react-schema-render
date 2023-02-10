0dda855ca44462e04e77a854fadcaa6b4b0f2ede
CaiYansong <caiyansong163@163com>
2023-02-10 11:28:36 +0800

    fix: pc form render radio props

--- packages/pc/form-render/components/radio/index.jsx
+++ packages/pc/form-render/components/radio/index.jsx
@@ -3,10 +3,19 @@ import { Form, Radio as Ra } from "antd";
 const FormItem = Form.Item;
 
 function Radio(props) {
-  const { formItemProps = {}, field = { options } } = props;
+  const { formItemProps = {}, field = { options }, data } = props;
+
+  const _props = {
+    id: field.name,
+    value: data,
+    disabled: field.disabled || props.disabled,
+    readOnly: field.readonly || props.readOnly,
+    onChange: props.onChange,
+  };
+
   return (
     <FormItem {...formItemProps}>
-      <Ra.Group onChange={props.onChange} value={props.data}>
+      <Ra.Group {..._props}>
         {field.options?.map((it) => (
           <Ra value={it.value}>{it.label}</Ra>
         ))}
--- src/pages/form-test/index.jsx
+++ src/pages/form-test/index.jsx
@@ -8,6 +8,7 @@ import demoSchema from "./demo.schema.json";
 
 export default function FormTestPage() {
   const [form, setForm] = useState({
+    "radio-c3fb0f5": "option2",
     "input-f9b7f9b": "1",
     "input-59f411d": "2",
     "select-b836c9c": ["option1"],
